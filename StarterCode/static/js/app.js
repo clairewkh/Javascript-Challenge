9901f2dfdbbea5f60339a8797373abaacdd55cf8
clairewkh <72780095+clairewkh@users.noreply.github.com>
2021-04-03 03:59:02 -0400

    Adding table data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// YOUR CODE HERE
+var tbody = d3.select("tbody");
+
+// forEach loop that populates table
+tableData.forEach((i) =>
+{
+    // row is created for table
+    var row = tbody.append('tr');
+    
+    row.append('td').text(i.datetime);
+    row.append('td').text(i.city);
+    row.append('td').text(i.state);
+    row.append('td').text(i.country);
+    row.append('td').text(i.shape);
+    row.append('td').text(i.durationMinutes);
+    row.append('td').text(i.comments);
+}
+)
